b6b9a62a6a679f6308cd9fc630e49a1189ca0fac
kumarnutan204 <kumarnutan204@gmail.com>
2023-04-27 13:33:15 +0530

    a complete vehicle management project

---
+++ vehicle/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+# from.models import Customer,Mechanic,Request,Feedback,Attendance
+
+
+# admin.site.register(Customer)
+# admin.site.register(Mechanic)
+# admin.site.register(Request)
+# admin.site.register(Feedback)
+# admin.site.register(Attendance)
+# Register your models here.
